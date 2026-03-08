1f90d0b60ef27f8dc37b572bd36b8bef1e1fdf5a
wonsik99 <algrthm@umich.edu>
2026-03-08 01:44:07 -0500

    Complete DocumentGPT

---
+++ Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(page_title="Fullstack GPT", page_icon="🤖")
+
+st.markdown(
+    """
+    # Hello!
+
+    Welcome to my FullstackGPT Portfolio!
+            
+    Here are the apps I made:
+                
+    - [x] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+    """
+)
+
