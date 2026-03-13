fa67398e0470534ba828b9c4ce6556623ce84079
wonsik99 <algrthm@umich.edu>
2026-03-12 23:57:54 -0400

    create QuizGPT

---
+++ pages/3_QuizGPT.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="QuizGPT", page_icon="❓")
+
+st.title("QuizGPT")
+
+st.markdown(
+    """
+    Welcome to QuizGPT.
+                
+    QuizGPT allows you to generate quizzes from documents or topics using AI.
+    """
+)
