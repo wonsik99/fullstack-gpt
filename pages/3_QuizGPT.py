8d2a438d1001c6f6a448f1d810293d9c74672e2e
wonsik99 <algrthm@umich.edu>
2026-03-15 16:56:45 -0400

    Update QuizGPT

--- pages/3_QuizGPT.py
+++ pages/3_QuizGPT.py
@@ -1,13 +1,217 @@
 import streamlit as st
+import json
+from langchain_community.retrievers import WikipediaRetriever
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.callbacks import StreamingStdOutCallbackHandler
+from langchain_core.output_parsers import BaseOutputParser
+
+class JsonOutputParser(BaseOutputParser):
+    def parse(self, text):
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+
+output_parsers = JsonOutputParser()
+    
 
 st.set_page_config(page_title="QuizGPT", page_icon="❓")
 
 st.title("QuizGPT")
 
-st.markdown(
-    """
-    Welcome to QuizGPT.
-                
-    QuizGPT allows you to generate quizzes from documents or topics using AI.
-    """
+llm = ChatOpenAI(
+    model_name="gpt-5-mini",
+    temperature=1,
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()]
 )
+
+question_prompt = ChatPromptTemplate.from_messages([
+    ("system",
+    """
+    You are a helpful assistant that is role playing as a teacher.
+
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+    Use (o) to signal the correct answer.
+
+    Question examples:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+    Your turn!
+
+    Context: {context}
+    """
+    )
+])
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+question_chain = {"context": format_docs} | question_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+    """
+    You are a powerful formatting algorithm.
+
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+
+    Example Output:
+
+    ```json
+    {{
+      "questions": [
+        {{
+          "question": "What is the color of the ocean?",
+          "answers": [
+            {{ "answer": "Red", "correct": false }},
+            {{ "answer": "Yellow", "correct": false }},
+            {{ "answer": "Green", "correct": false }},
+            {{ "answer": "Blue", "correct": true }}
+          ]
+        }},
+        {{
+          "question": "What is the capital or Georgia?",
+          "answers": [
+            {{ "answer": "Baku", "correct": false }},
+            {{ "answer": "Tbilisi", "correct": true }},
+            {{ "answer": "Manila", "correct": false }},
+            {{ "answer": "Beirut", "correct": false }}
+          ]
+        }},
+        {{
+          "question": "When was Avatar released?",
+          "answers": [
+            {{ "answer": "2007", "correct": false }},
+            {{ "answer": "2001", "correct": false }},
+            {{ "answer": "2009", "correct": true }},
+            {{ "answer": "1998", "correct": false }}
+          ]
+        }},
+        {{
+          "question": "Who was Julius Caesar?",
+          "answers": [
+            {{ "answer": "A Roman Emperor", "correct": true }},
+            {{ "answer": "Painter", "correct": false }},
+            {{ "answer": "Actor", "correct": false }},
+            {{ "answer": "Model", "correct": false }}
+          ]
+        }}
+      ]
+    }}
+    ```
+    Your turn!
+    Questions: {context}
+    """
+    )
+])
+
+formatting_chain = formatting_prompt | llm
+
+@st.cache_resource(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+@st.cache_resource(show_spinner="Loading Wikipedia...")
+def run_quiz_chain(_docs, topic):
+    chain = {"context": question_chain} | formatting_chain | output_parsers
+    response = chain.invoke(docs)
+    return response
+
+@st.cache_resource(show_spinner="Searching Wikipedia...")
+def wiki_serach(term):
+    retriever = WikipediaRetriever(top_k_results=5)
+    docs = retriever.invoke(term)
+    return docs
+    
+
+
+
+with st.sidebar:
+    docs = None
+    choice = st.selectbox(
+        "Choose a quiz type",
+        ("File", "Wikipedia Article")
+    )
+
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .txt .pdf of .docx file",
+            type=["pdf", "txt", "docx"]
+        )
+        if file:
+            docs = split_file(file)
+
+    elif choice == "Wikipedia Article":
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            docs = wiki_serach(topic)
+        
+if not docs:
+    st.markdown(
+        """
+        Welcome to QuizGPT!
+        
+        Upload a document or enter a Wikipedia topic to get started.
+        """
+    )
+
+    response = run_quiz_chain(docs, topic if topic else file.name)
+    with st.form("questions_form"):
+        for question in response["questions"]:
+            st.write(question["question"])
+            value = st.radio(
+                "Select an option.",
+                [answer["answer"] for answer in question["answers"]],
+                index=None,
+            )
+            if {"answer": value, "correct": True} in question["answers"]:
+                st.success("Correct!")
+            elif value is not None:
+                st.error("Wrong!")
+        button = st.form_submit_button()
+        
+        
