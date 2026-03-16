2ffe56af9132e29646bc9acfa2f205285c48c97f
wonsik99 <algrthm@umich.edu>
2026-03-16 01:14:25 -0400

    Create SiteGPT

---
+++ pages/4_SiteGPT.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="SiteGPT", page_icon="🌐")
+
+st.title("SiteGPT")
+
+st.markdown(
+    """
+    Welcome to SiteGPT.
+                
+    SiteGPT allows you to chat with the contents of any website using AI.
+    """
+)
