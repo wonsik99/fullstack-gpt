b957459fff38778b3520fc3bc7ead36438262098
wonsik99 <algrthm@umich.edu>
2026-03-21 18:01:23 -0400

    SiteGPT load and split using BeautifulSoup

--- pages/4_SiteGPT.py
+++ pages/4_SiteGPT.py
@@ -1,13 +1,36 @@
 import streamlit as st
-from langchain_community.document_loaders import AsyncChromiumLoader
-from langchain_community.document_transformers import Html2TextTransformer
-# from langchain_community.document_loaders import SitemapLoader
+from langchain_community.document_loaders import SitemapLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return soup.get_text()
+
+@st.cache_resource(show_spinner="Loading website...")
+def load_website(url):
+
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000,
+    chunk_overlap=200
+    )
+
+    loader = SitemapLoader(
+        url,
+        filter_urls=[r"^(.*\/api).*$"],
+        parsing_function=parse_page,
+        requests_per_second=0.1
+    )
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
 
 st.set_page_config(page_title="SiteGPT", page_icon="🌐")
 
 st.title("SiteGPT")
-
-html2text_transformer = Html2TextTransformer()
 
 st.markdown(
     """
@@ -26,11 +49,12 @@ with st.sidebar:
     )
 
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(transformed)
-    
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down Sitemap URL")
+    else:
+        docs = load_website(url)
+        st.write(docs)
 
 
     
